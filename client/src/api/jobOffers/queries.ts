84813572177cb0eaf05599cd16b0137b9b78eb4d
bartek3pl <bartekh1998@op.pl>
2021-07-31 19:58:42 +0200

    feat: implement first client query for fetching job offers

---
+++ client/src/api/jobOffers/queries.ts
@@ -0,0 +1,51 @@
+import { gql } from '@apollo/client';
+
+export const GET_ALL_JOB_OFFERS = gql`
+  query getAllJobOffers(
+    $offset: Int
+    $first: Int
+    $sorting: String
+    $search: JobOfferSearch
+  ) {
+    getAllJobOffers(
+      offset: $offset
+      first: $first
+      sorting: $sorting
+      search: $search
+    ) {
+      code
+      message
+      success
+      results {
+        pageInfo {
+          totalCount
+          hasMore
+        }
+        jobOffers {
+          _id
+          title
+          description
+          employer {
+            name
+            address {
+              country
+              city
+              street
+              postalCode
+              buildingNumber
+              apartmentNumber
+            }
+            employeesNumber
+            logo
+          }
+          minSalary
+          maxSalary
+          skills
+          experienceYears
+          levels
+          contractType
+        }
+      }
+    }
+  }
+`;
